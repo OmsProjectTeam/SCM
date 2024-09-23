1c993a0262241152c4dbb0e5e2dba12c360885fc
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-09-23 21:46:12 +0600

    update

---
+++ Yara/wwwroot/js/merchant-image.js
@@ -0,0 +1,32 @@
+﻿// merchants.js
+
+$(document).ready(function () {
+    $('.select2_1').select2({
+        placeholder: "Select an option",
+        allowClear: true
+    });
+
+    $('#merchant-select').change(function () {
+        var userId = $(this).val();
+        if (userId) {
+            $.getJSON('@Url.Action("GetUserDetails", "Merchants")', { id: userId }, function (data) {
+                $('input[name="Merchants.CompanyName"]').val(data.companyName);
+                $('input[name="Merchants.PhoneCompany"]').val(data.phoneCompany);
+                $('input[name="Merchants.PhoneCompanySecand"]').val(data.phoneCompanySecand);
+                $('input[name="Merchants.EmailCompany"]').val(data.emailCompany);
+                $('input[name="Merchants.MerchantPhone"]').val(data.merchantPhone);
+                $('input[name="Merchants.MerchantEmaile"]').val(data.merchantEmaile);
+            });
+        } else {
+            $('input[name="Merchants.MerchantPhone"]').val('');
+            $('input[name="Merchants.MerchantEmaile"]').val('');
+        }
+    });
+
+    var loadFile = function (event) {
+        var image = document.getElementById('output');
+        image.src = URL.createObjectURL(event.target.files[0]);
+    };
+
+    $("#url").val(window.location.href);
+});
---
+++ Yara/wwwroot/js/my-product-information.js
@@ -0,0 +1,77 @@
+﻿// Ensure the QR code is updated on page load and after pagination
+$(document).ready(function () {
+    updateQRCode();  // Call the function on page load to generate QR codes
+});
+
+
+$(document).ready(function () {
+    updateBar();  // Call the function on page load to generate QR codes
+});
+
+$(document).on('click', '.paginate_button', function () {
+    updateBarCode();  // Update QR code when pagination is clicked
+});
+
+// Function to update the QR code image
+function updateQRCode() {
+    $('.QRCodeImage').each(function () {
+        var code = $(this).siblings('.Code').text();
+        var generateQRCodeUrl = $(this).data('url'); // Use data attribute to get the URL
+        if (code.trim() !== '') {
+            $(this).attr('src', generateQRCodeUrl + '?text=' + encodeURIComponent(code));
+        } else {
+            $(this).attr('src', '');  // Set empty if no valid code
+        }
+    });
+}
+
+function updateBarCode() {
+    $('.BarCodeUPC').each(function () {
+        var upc = $(this).siblings('.UPCFORPR').text().trim();
+        if (upc) {
+            $(this).attr('src', urls.generateBarCode + '?text=' + encodeURIComponent(upc));
+        } else {
+            $(this).attr('src', '');
+        }
+    });
+}
+
+//$(document).ready(function () {
+//    const urls = {
+//        generateQRCode: $('#url-container').data('qrcode-url'),
+//        generateBarCode: $('#url-container').data('barcode-url')
+//    };
+
+//    // Use the URLs as needed
+//    function updateQRCode() {
+//        $('.QRCodeImage').each(function () {
+//            var code = $(this).siblings('.Code').text().trim();
+//            if (code) {
+//                $(this).attr('src', urls.generateQRCode + '?text=' + encodeURIComponent(code));
+//            } else {
+//                $(this).attr('src', '');
+//            }
+//        });
+//    }
+
+//    function updateBar() {
+//        $('.BarCodeUPC').each(function () {
+//            var upc = $(this).siblings('.UPCFORPR').text().trim();
+//            if (upc) {
+//                $(this).attr('src', urls.generateBarCode + '?text=' + encodeURIComponent(upc));
+//            } else {
+//                $(this).attr('src', '');
+//            }
+//        });
+//    }
+
+//    // Call functions when the document is ready
+//    updateQRCode();
+//    updateBar();
+
+//    // Update QR and Bar Codes on pagination clicks
+//    $(document).on('click', '.paginate_button', function () {
+//        updateQRCode();
+//        updateBar();
+//    });
+//});
